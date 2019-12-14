1b8a123a1457c3b831fbe97a7ca798448b0a38b1
TOSHIBA <TOSHIBA@TOSHIPC>
2019-12-14 13:43:38 -0500

    Lavadora indurama tc

---
+++ src/lavadora/LavadoraUno.java
@@ -0,0 +1,23 @@
+
+package lavadora;
+
+import libreria.LLFunciones1;
+import java.util.Scanner;
+
+
+public class LavadoraUno {
+    public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("La ropa es blanco o de color");
+        System.out.println("Presione 1 si es blanca / presione 2 si es de color");
+        int tipodeRopa = entrada.nextInt();
+        System.out.println("Cuantos Kilos de ropa");
+        int kilos = entrada.nextByte();
+        LLFunciones1 f = new LLFunciones1(kilos, tipodeRopa);
+        f.setTipoRopa(tipodeRopa);
+        System.out.println("el tipo de ropa es: "+f.getTipoRopa());
+        f.cicloFinalizado();
+    }
+    
+}
---
+++ src/libreria/LLFunciones1.java
@@ -0,0 +1,117 @@
+
+package libreria;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+public class LLFunciones1 {
+    private int kilos = 0, tipodeRopa = 0, llenadocompleto = 0, lavadocompleto = 0, secadocompleto = 0;
+    
+    public LLFunciones1(int kilos, int tipodeRopa){
+        this.kilos = kilos;
+        this.tipodeRopa = tipodeRopa;
+      }
+
+    private void llenado(){
+        
+        if (kilos <=20){
+            //codigo
+            
+                    llenadocompleto = 1;//averiguar lo que es una bandera
+                    
+                    Timer s = new Timer();
+            TimerTask tarea = new TimerTask() {
+                
+                @Override              
+                public void run() {
+            System.out.println("Llenando.....");
+            
+             }
+                
+            };
+            s.schedule(tarea, 3000);
+           } 
+            
+        
+        else{
+            Timer s = new Timer();
+            TimerTask tarea = new TimerTask() {
+                
+                @Override              
+                public void run() {
+            System.out.println("Llenado completo");
+              //codigo
+             }
+                
+            };
+            s.schedule(tarea, 3000);
+            
+           }   
+         
+        
+            System.out.println("La carga sobrepasa el peso, disminuya la misma");
+                      
+                       
+    }
+    
+    private void lavado(){
+        llenado();
+        
+        if (llenadocompleto==1){
+            
+            if(tipodeRopa==1){
+                System.out.println("Ropa blanca / Lavado suave");
+                System.out.println("Lavando....");
+                lavadocompleto = 1;
+            }else if(tipodeRopa==2){
+                
+                System.out.println("Ropa de color / lavado intenso");
+                System.out.println("Lavando...");
+                lavadocompleto = 1;
+            }else{
+                System.out.println("No ha considerado el tipo de ropa");
+                System.out.println("Se lavara como ropa de color / Lavado intenso");
+                System.out.println("Lavando");
+                lavadocompleto = 1;
+            }
+            
+        }    
+    }
+    
+    private void secado(){
+        lavado();
+        if (lavadocompleto==1){
+            System.out.println("Secando...");
+            secadocompleto = 1;
+            
+        }
+    }
+    public void cicloFinalizado(){
+        secado();
+        if (secadocompleto==1){
+            System.out.println("La ropa esta lista");
+        }
+    }
+    
+    public void setTipoRopa (int tipodeRopa){
+        this.tipodeRopa = tipodeRopa;
+    }
+    
+    
+    public int getTipoRopa (){
+        return tipodeRopa;
+    }
+    
+     public void setKilos(int kilos) {
+        this.kilos = kilos;
+    }
+        
+    public int getKilos() {
+        return kilos;
+    }
+
+
+    
+    
+}
